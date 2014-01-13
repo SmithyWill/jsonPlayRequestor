c61c94b6e4d358c459782ab43e0571f9d8bae568
Will Smith <william.smith@accenture.com>
2014-01-13 06:01:20 +0000

    Added comments to code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,7 +15,7 @@ import models.User
 import models.UserReturn
 
 
-
+//The index page
 object Application extends Controller {
 
   def index = Action {
@@ -30,12 +30,15 @@ object Application extends Controller {
     "id" -> text)(User.apply)(User.unapply))
   
   
+  //Required for future Result call. 
   implicit val context = scala.concurrent.ExecutionContext.Implicits.global
-	  
-  implicit val personReads: Reads[User] = (
+  
+  //How to read a user object. 
+  implicit val userReads: Reads[User] = (
   (__ \ "name").read[String]
   and (__ \ "id").read[String])(User)
-	   
+
+  //How to read the user returned object. 
   implicit val userReturnReads: Reads[UserReturn] = (
   (__ \ "name").read[String]
   and (__ \ "id").read[String] 
@@ -43,30 +46,39 @@ object Application extends Controller {
   and (__ \ "gender").read[String] 
   and (__ \ "address").read[String])(UserReturn)
   
-	   
+  /*The json form to display, the empty form object is passed to the view in order to bind the fields
+   * on the form to the field variables. 
+   */
   def jsonForm = Action {   
-    Ok(views.html.jsonForm(userForm))  
+	  Ok(views.html.jsonForm(userForm))  
   }   
   
-  def sendRequest = Action.async {
+  //The method to send a request to the play service expecting a json request. 
+  def sendRequest = Action.async { implicit request => //gets the request object from the browser. 
 	  
-	  implicit request =>
-	  val userID : String = userForm.bindFromRequest.get.id
-	  val userName : String = userForm.bindFromRequest.get.name
+      val userID : String = userForm.bindFromRequest.get.id //get the id property 
+	  val userName : String = userForm.bindFromRequest.get.name//get the name property
       
-	  val data = Json.obj("name" -> userName,"age" -> userID)
+	  val data = Json.obj("name" -> userName,"age" -> userID)// construct a json object 
 	  
-	  val url = "http://localhost:9001/jsonRespond"
-   	  	  
+	  val url = "http://localhost:9001/jsonRespond" //the service url
+   	  
+	  
+	  /* The post method will return an object of type response  
+	   * response => .... states take the response and apply it to the following anonymous function
+	   * .validate will attempt to convert the response and map it into a UserReturn object. 
+	   * The validate method will return a JsResult object that can either be JsSuccess (conversion succeeded) or JsError (conversion failed).
+	   * Depending on the case either the Ok Result with the form submitted html is returned or an OK result with a simple error message. 
+	   */
    	  val futureResult = WS.url(url).post(data).map {response => (response.json).validate[UserReturn] match {
         case JsSuccess(user, _) =>  
           Ok(views.html.formSubmitted(user))
         case JsError(errors) => 
-          Ok("There are errors in JSON")
+          Ok("There are errors in the JSON")
       } 
     }
-	  
-	futureResult 
+	  	//The result is returned. 
+		futureResult 
 
   }
   
